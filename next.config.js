f5242c9fc8ea5a4220d9be70224c2d282654bcd1
Maxime Palmino <maximepalmino.pro@gmail.com>
2023-12-25 17:12:07 +0100

    [UPDATE]back to useEffect

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     webpack(config, options) {
-        const withTM = require("next-transpile-modules")(["gsap"]);
+
         const {isServer} = options;
         config.module.rules.push({
             test: /\.(ogg|mp3|wav|mpe?g)$/i,
@@ -26,5 +26,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-module.exports = withTM({});
-
